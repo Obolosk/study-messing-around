cd358fca8e73421c8c917da80e28a0ae68032e38
Obolosk <minhducphamngoc1@gmail.com>
2025-09-10 08:43:51 +0700

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,36 +1,37 @@
 #include <bits/stdc++.h>
 
-
 using namespace std;
 
 
-int task(vector<int> &a, int n) {
-    int c = 1e7;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (abs(a[i] + a[j]) < abs(c)) {
-                c = a[i] + a[j];
-            }
-        }
-    }
-    return c;
+bool ok = true;
+void next_bit_string(string& x){
+	int i = x.length()-1;
+		
+	while(i >= 0 && x[i] != 'A'){
+		x[i] = 'A';
+		i--;
+	}
+	if(i >= 0) x[i] = 'B';
+	else ok = false;
+	
 }
+
 
 int main() {
 	int t; cin >> t;
-	while(t--) {
+	while(t--){
+		ok = true;
 		int n; cin >> n;
-		vector<int> a(n);
 		
-		for(int i = 0; i < n; i++) {
-			cin >> a[i];
+		string x(n, 'A');
+		
+		
+		while(ok){
+			cout << x << ' ';
+			next_bit_string(x);
 		}
-		
-		
-		
-		cout << task(a, n) << "\n";
-	}
 
+		cout << "\n";
+	}
 	return 0;
 }
-
